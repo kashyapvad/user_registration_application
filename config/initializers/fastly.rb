e873757b919c3d3ea04eb14ec15a3167186fe29c
kashyapvad <kashyap.vad@gmail.com>
2021-01-26 21:34:36 -0800

    update fastly key

--- config/initializers/fastly.rb
+++ config/initializers/fastly.rb
@@ -1,6 +1,6 @@
 FastlyRails.configure do |config|
-  config.api_key = ENV.fetch("q6XGdBYyq_t7d4pG2OvKPzUJM4bz5x2S")
+  config.api_key = "q6XGdBYyq_t7d4pG2OvKPzUJM4bz5x2S"
   thirty_days_in_seconds = 2592000
   config.max_age = thirty_days_in_seconds
-  config.service_id = ENV.fetch("3DrLTPGFKzrJMEk58x2OeT")
+  config.service_id = "3DrLTPGFKzrJMEk58x2OeT"
 end
