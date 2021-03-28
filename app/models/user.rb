dc4171fa43172cc33a5288b877963e6096f3f3f2
Kash <kashyap.vad@gmail.com>
2021-03-28 13:54:39 -0700

    remove fastly

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,8 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 8}
   validates :email, presence: true, uniqueness: true, :format => {:with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "address not valid"}
 
-  after_save :purge_fastly
-
   def set_username
     self.email = self.email.downcase
     self.username = self.email.split('@').first
-  end
-
-  def purge_fastly
-    self.purge
   end
 end
